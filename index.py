8bfa1a7496637f0efd65f1b492552d27136c351d
arkarsg <alxarkar@gmail.com>
2024-08-01 17:53:51 +0800

    Limit memory usage

--- index.py
+++ index.py
@@ -1,8 +1,16 @@
 from app import run
 import streamlit as st
 import asyncio
+import psutil
+import resource
 
 if __name__ == "__main__":
+    virtual_memory = psutil.virtual_memory()
+    available_memory = virtual_memory.available
+    memory_limit = int(available_memory * 0.98)
+    # Set the memory limit
+    resource.setrlimit(resource.RLIMIT_AS, (memory_limit, memory_limit))
+    
     st.set_page_config(
         page_title="CrateDigger",
         page_icon="🐡",
